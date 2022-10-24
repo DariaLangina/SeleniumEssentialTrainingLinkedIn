15256afebb24e98bdfcfd9257e5da8e504be0a0d
Daria Langina <langina.ds@mail.ru>
2022-10-24 14:52:22 +0400

    06_03 done

--- src/test/java/dlangina/from_06/Form.java
+++ src/test/java/dlangina/from_06/Form.java
@@ -3,7 +3,6 @@ package dlangina.from_06;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -18,8 +17,16 @@ public class Form {
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://formy-project.herokuapp.com/form");
+        submitForm(driver);
 
-//        in the lesson
+        waitForAlertBanner(driver);
+        getAlertBannerText(driver);
+        assertEquals("The form was successfully submitted!", getAlertBannerText(driver));
+
+        driver.quit();
+    }
+
+    public static void submitForm(WebDriver driver) {
         driver.findElement(By.id("first-name")).sendKeys("Daria");
         driver.findElement(By.id("last-name")).sendKeys("Langina");
         driver.findElement(By.id("job-title")).sendKeys("QA");
@@ -30,13 +37,14 @@ public class Form {
         driver.findElement(By.id("datepicker")).sendKeys("11/11/1991");
         driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN);
         driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
+    }
 
+    public static void waitForAlertBanner(WebDriver driver) {
         WebDriverWait wait = new WebDriverWait(driver, 10);
-        WebElement alert = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("alert")));
-        String alertText = alert.getText();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("alert")));
+    }
 
-        assertEquals("The form was successfully submitted!", alertText);
-
-        driver.quit();
+    public static String getAlertBannerText(WebDriver driver) {
+        return driver.findElement(By.className("alert")).getText();
     }
 }
